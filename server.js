1d9cb4c4bfad5bc9f6f1dda5aa27e19ebefd7e4f
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-26 12:38:04 -0600

    Added dynamic error handling

--- server.js
+++ server.js
@@ -20,15 +20,29 @@ app.get('/', (req, res) => {
     rollbar.info('html file served successfully')
 })
 
+app.get('/style', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/style.css'))
+})
+
 let students = []
 
 app.post('/api/student', (req, res) => {
     let {name} = req.body
     name = name.trim()
 
+   const index = students.findIndex(studentName => studentName === name)
+
+   if(index === -1 && name !== '') {
     students.push(name)
     rollbar.log('Student added successfully', {author: 'Dillon', type: 'Manual Entry'})
     res.status(200).send(students)
+   } else if(name === '') {
+       rollbar.error('No name given')
+       res.status(400).send("must Provide a name")
+   } else {
+       rollbar.critical("Student already exists")
+       res.status(400).send('That student already exists')
+   }
 })
 
 app.use(rollbar.errorHandler())
